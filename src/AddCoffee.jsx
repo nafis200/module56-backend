ab80a38555fc2aaf2731e0bc213890e9fd630b88
Nafis Ahamed <nafisahamed14@gmail.com>
2024-04-21 21:26:59 +0600

    second commit

--- src/AddCoffee.jsx
+++ src/AddCoffee.jsx
@@ -1,9 +1,24 @@
 const AddCoffee = () => {
-  return (
-    <div>
-      <h2 className="text-2xl">Add coffee</h2>
 
-      <form>
+  const handlePrevent = event =>{
+    event.preventDefault();
+    const form = event.target
+    const quantity = form.quantity.value;
+    const name = form.name.value;
+    const supplier = form.supplier.value;
+    const test = form.test.value;
+    const category = form.category.value;
+    const photo = form.photo.value;
+    const details = form.details.value;
+    console.log(quantity,name,supplier,test,category,photo,details);
+
+  }
+
+  return (
+    <div className="bg-[#F4F3F0] p-24">
+      <h2 className="text-2xl font-bold mb-5">Add coffee</h2>
+
+      <form onSubmit={handlePrevent}>
         <div className="flex gap-5">
         <div className="form-control">
           <label className="input-group input-group-vertical">
@@ -11,7 +26,8 @@ const AddCoffee = () => {
             <input
               type="text"
               placeholder="coffee Name"
-              className="input input-bordered"
+              className="input input-bordered w-full"
+              name = "name"
             />
           </label>
         </div>
@@ -21,11 +37,75 @@ const AddCoffee = () => {
             <input
               type="text"
               placeholder="Available quantity"
-              className="input input-bordered"
+              className="input input-bordered w-full"
+              name = "quantity"
             />
           </label>
         </div>
         </div>
+        <div className="flex gap-5">
+        <div className="form-control">
+          <label className="input-group input-group-vertical">
+            <span>Supplier</span>
+            <input
+              type="text"
+              placeholder="supplier name"
+              className="input input-bordered w-full"
+              name = "supplier"
+            />
+          </label>
+        </div>
+        <div className="form-control">
+          <label className="input-group input-group-vertical">
+            <span>test</span>
+            <input
+              type="text"
+              placeholder="test"
+              className="input input-bordered w-full"
+              name = "test"
+            />
+          </label>
+        </div>
+        </div>
+        <div className="flex gap-5">
+        <div className="form-control">
+          <label className="input-group input-group-vertical">
+            <span>category</span>
+            <input
+              type="text"
+              placeholder="category"
+              className="input input-bordered w-full"
+              name = "category"
+            />
+          </label>
+        </div>
+        <div className="form-control">
+          <label className="input-group input-group-vertical">
+            <span>details</span>
+            <input
+              type="text"
+              placeholder="details"
+              className="input input-bordered w-full"
+              name = "details"
+            />
+          </label>
+        </div>
+        </div>
+        <div className="">
+        <div className="form-control">
+          <label className="input-group input-group-vertical mt-5">
+            <span className="">photo</span>
+            <input
+              type="text"
+              placeholder="photo url"
+              className="input input-bordered w-full mt-2"
+              name = "photo"
+            />
+           
+          </label>
+        </div>
+        </div>
+        <input type="submit" value="Add coffee" className="btn btn-primary w-full mb-5 mt-5"/>
       </form>
     </div>
   );
